0a9ecdc5648fa2aa8f3e6b2311250dff2340dc06
xh753 <129875984+xh753@users.noreply.github.com>
2023-04-07 14:47:52 -0400

    stats

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -51,6 +51,7 @@ class MarkdownParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
 
 
 class ReStructuredTextParser(Parser):
@@ -64,3 +65,4 @@ class ReStructuredTextParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
